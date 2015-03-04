f5a2f2f94474d8de4f6f9b4c48eefada5143f17d
Alex <d3t0ur@hotmail.com>
2015-03-04 01:05:22 -0500

    Implemented the all-in feature and add git-ignores

--- Comp345_Asg1_Battle/Comp345_Asg1_BattleLast/battle.h
+++ Comp345_Asg1_Battle/Comp345_Asg1_BattleLast/battle.h
@@ -1,5 +1,5 @@
 //#include "stdafx.h"
-//#include "player.h"
+#include "country.h"
 
 #include <string>
 using namespace std;
@@ -12,13 +12,50 @@ public:
 	// Default constructor
 	Battle();
 	// Parametrized constructor
-	Battle(Player pAtkr, Player pDfndr);
+	Battle(Country *cAtkr, Country *cDfndr);
 
 	// Public methods Declarations
 
 	// Public variables declaration
-	Player playerAtkr;
-	Player playerDfndr;
+	Country *countryAtkr;
+	Country *countryDfndr;
+
 private:
+	// Private methods Declarations
+
+	// Main battle function with the two conflicting coutries as input
+	void battleDrive(Country *countryDfndr, Country *countryAtkr);
+
+	// Function to fing highest value in input array
+	int maximumValue(int a[]);
+
+	// Determines maximum attack roll allowance based on number of armies in country
+	int availableAttackType(Country cAtkr);
+
+	// User choice of attack
+	int attackChoice(int maxRollNum);
+
+	// Performs the selected number of roles and stores them in array
+	int* attackRoll(int numOfAtckRolls); 
+
+	// Determines maximum number of defence rolls
+	int* defenseRoll(int maxRollNum);
+
+	// Determines best roll in provide array
+	int getBestRoll(int arr[]);
+
+	// Determines second best roll in provide array
+	int getSecondBestRoll(int* arr, int currentMax);
+
+	// Determines winner of the round
+	string getWinner(int maxAtkr, int maxDfndr);
+
+	// Determines the max attacker roll automatically during All-in
+	int maxAttackType(Country cAtkr); 
+
+	// All in attack feature until conquer or loss
+	void allIn(Country *cAtkr, Country *cDfndr);
+
+	// Private variables declaration
 
 };
