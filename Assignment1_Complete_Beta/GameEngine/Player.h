a2debdf53987448fd565009ad0cdf0291650a17d
Alex <d3t0ur@hotmail.com>
2015-03-05 08:20:37 -0500

    Added the Complete Beta project & modification to engine

---
+++ Assignment1_Complete_Beta/GameEngine/Player.h
@@ -0,0 +1,35 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+#include <hash_map>
+#include <vector>
+#include "Country.h"
+
+using namespace std;
+
+class Player
+{
+private:
+	string playerName;
+	int remainingArmies; //Unused armies.
+	vector<Country> *countriesOwned; //Countries owned/conquered by the player.
+
+public:
+	Player(string Name, int Armies); 
+	string getPlayerName();
+	vector<Country> getCountriesOwned();
+	int getRemainingArmies();
+	Country* getCountry(int index); //Returns the address of the element at vector[index]
+
+	void IncrementNumberOfArmies(int Armies);
+	void DecrementNumberOfArmies(int Armies);
+	void AddCountry(Country country); //Add country to the list of countries owned.
+	void PrintPlayerStats(); //Display
+};
+
+#endif
+
+
+
+
+
---
+++ Assignment1_Complete_Beta/GameEngine/StartupPhase.h
@@ -0,0 +1,31 @@
+#ifndef STARTUPPHASE_H
+#define STARTUPPHASE_H
+#include <vector>
+#include "Player.h"
+#include "Country.h"
+
+using namespace std;
+
+class StartupPhase {
+
+private:
+	int numberOfPlayers; //Number of players chosen by the user.
+	vector <Player> *players; //List of players
+	vector<Country> countries; //List of all the countries in the map.
+
+	int getIntialArmyPlacements(int NumberOfPlayers); //Computes the intial number of armies according to the numberOfPlayers.
+	void ShuffleCountries(); //Shuffle elements in the countries vector.
+	void PrintPlayerInfo(); //Display
+
+public:
+	StartupPhase(int NoOfPlayers); 
+
+	int getNumberOfPlayers();
+	vector <Player> getPlayers();
+	vector <Country> getCountries();
+	void StartGame();
+	void ChooseMap();
+	void CreatePlayers();
+	void AssignCountries();
+};
+#endif
